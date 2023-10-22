14ad2d47115f0ccac3d9026641a614b2a54c8962
19Aleksandra95 <polar.heder@gmail.com>
2023-10-22 21:54:40 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,110 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { nanoid } from 'nanoid';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import css from './App.module.css';
+
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+
+//LocalStorage
+
+  componentDidMount() {
+    const localStorageData= JSON.parse(localStorage.getItem('contacts'))
+    if(localStorageData) {
+      this.setState({contacts: localStorageData});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts.length));
+    }
+  }
+
+
+  //Sposób przetwarzania formularza - dodanie danych do stanu (dane pobierane są z komponentu ContactForm)
+  formSubmitHandler = data => {
+    const { contacts } = this.state;
+    console.log(data);
+    //Uniemożliwia dodawanie kontaktów, których nazwy znajdują się już w książce telefonicznej.
+    if (contacts.some(contact => contact.name === data.name)) {
+      alert(`${data.name} is already in contacts.`);
+      return;
+    }
+    this.setState({
+      contacts: [
+        ...contacts,
+        { id: this.generetedId(), name: data.name, number: data.number },
+      ],
+    });
+  };
+
+  //Funkcja nanoid() pobiera opcjonalny argument określający długość id
+  generetedId = () => {
+    return nanoid(5);
+  };
+
+  // Metoda aktualizacji pola filtru
+  handleChangeFilter = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  //Metoda filtracji kontaktów
+  getFilteredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  //Sposób usuwania kontaktu z listy kontaktów
+  deleteContact = deleteId => {
+    this.setState(PrevState => ({
+      contacts: PrevState.contacts.filter(contact => contact.id !== deleteId),
+    }));
+    this.setState({ filter: '' });
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#010101',
+        }}
+      >
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2 className={css.subtitle}>Contacts</h2>
+        <p className={css.total}>
+          Total contacts:
+          <span className={css.total_count}> {this.state.contacts.length}</span>
+        </p>
+        <Filter value={this.state.filter} onChange={this.handleChangeFilter} />
+        <ContactList
+          filteredContacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
